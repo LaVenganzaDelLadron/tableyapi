e4facb07d5930469629071332171fb3ee4b2af8b
argus <darkglitch5417@gmail.com>
2026-05-24 03:37:11 +0800

    updating the api login implementing prefix for auth

--- routes/api.php
+++ routes/api.php
@@ -33,8 +33,10 @@ Route::prefix('v1')->group(function () {
     |--------------------------------------------------------------------------
     */
 
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::prefix('auth')->group(function (): void {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+    });
 
     /*
     |--------------------------------------------------------------------------
