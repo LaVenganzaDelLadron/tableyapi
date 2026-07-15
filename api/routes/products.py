26f8dbd48b9cbd0e3eeccb0a40a0ce8f9c1c54bb
LaVenganzaDelLadron <darkglitch5417@gmail.com>
2026-07-15 11:58:31 +0800

    fix issue on api routes

--- api/routes/products.py
+++ api/routes/products.py
@@ -4,7 +4,10 @@ from sqlalchemy.orm import Session
 from api.dependencies import get_current_user, get_db
 from api.responses import success
 from schemas.products import Products
-from services.products_service import index, store
+from services.products_service import (
+    index as product_index,
+    store as product_store,
+)
 
 
 router = APIRouter()
@@ -12,9 +15,10 @@ router = APIRouter()
 
 @router.get("/")
 async def index(db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    data = index(db)
+    data = product_index(db)
     return success("Fetch data successfully", data)
 
 @router.post("/")
 async def store(payload: Products, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    data = store(db, payload.product_name, payload.selling_price, payload.current_stock)
+    data = product_store(db, payload.product_name, payload.selling_price, payload.current_stock)
+    return success("Fetch data successfully", data)
--- api/routes/raw_materials.py
+++ api/routes/raw_materials.py
@@ -4,17 +4,19 @@ from sqlalchemy.orm import Session
 from api.dependencies import get_current_user, get_db
 from api.responses import  success
 from schemas.raw_materials import RawMaterials
-from services.raw_materials_service import index, store
-
+from services.raw_materials_service import (
+    index as raw_materials_index,
+    store as raw_materials_store
+)
 
 router = APIRouter()
 
 @router.get("/")
-async def index(db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    data = index(db)
+async def index(db: Session = Depends(get_db), current_user=Depends(get_current_user)):
+    data = raw_materials_index(db)
     return success("Successfully fetched data", data)
 
 @router.post("/")
-async def store(payload: RawMaterials, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    data = store(db, payload.material_name, payload.quantity, payload.unit)
+async def store(payload: RawMaterials, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
+    data = raw_materials_store(db, payload.material_name, payload.quantity, payload.unit)
     return success("Successfully stored data", data)
